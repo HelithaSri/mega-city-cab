63eec2a9ab74a6ed44b593f1c3faa3ebdf190405
HelithaSri <helitha.pravin@gmail.com>
2025-03-01 18:50:03 +0530

    Driver save issue fix

--- src/main/java/lk/cab/manager/megacitycab/repository/DriverRepository.java
+++ src/main/java/lk/cab/manager/megacitycab/repository/DriverRepository.java
@@ -64,7 +64,7 @@ public class DriverRepository {
 
     public List<Driver> findAllByStatus(boolean status) {
         try {
-            ResultSet rs = CrudUtil.executeQuery(QueryUtil.FIND_ALL_DRIVERS_BY_STATUS,status);
+            ResultSet rs = CrudUtil.executeQuery(QueryUtil.FIND_ALL_DRIVERS_BY_STATUS, status);
             List<Driver> list = new ArrayList<>();
             while (rs.next()) {
                 list.add(CustomMapper.mapResultSetToEntity(rs, Driver.class));
@@ -80,7 +80,7 @@ public class DriverRepository {
     }
 
     public boolean save(Driver driver) throws SQLException {
-        boolean executed = CrudUtil.executeUpdate(QueryUtil.SAVE_DRIVER, driver.getId(), driver.getName(), driver.getAddress(), driver.getNic(), driver.getMobile(), driver.getEmail(), driver.getDob());
+        boolean executed = CrudUtil.executeUpdate(QueryUtil.SAVE_DRIVER, driver.getId(), driver.getName(), driver.getAddress(), driver.getNic(), driver.getDrivingLicence(), driver.getMobile(), driver.getEmail(), driver.getDob(), driver.isAvailability());
         if (!executed) {
             LOGGER.log(Level.WARNING, () -> "Failed to saved driver");
             return false;
